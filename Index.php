83d608558ac0af112a5ae5c1bc0a9338ad592229
sodiq2000 <48282579+sodiq2000@users.noreply.github.com>
2021-02-28 22:23:59 +0100

    Update Index.php

--- Index.php
+++ Index.php
@@ -4,10 +4,19 @@
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-<title>Basic form handling in php</title>
-</head>
-<body>
-
-</body>
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic form handling in php</title>
+  </head>
+  <body>
+     <div class="container">
+        <h1>Basic Php Form handling</h1>
+        <form method="POST" action="index.php">
+          <input type="text" name="user" placeholder="Enter your name...">
+          <input type="submit" name="submit" value="submit">
+        </form>
+     </div>
+  </body>
 </html>
